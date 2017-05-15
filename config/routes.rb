4ce5298b367340838f1e341a26344094089e3f85
Joan Merelos <joan@egissys.com>
2017-05-15 09:31:38 +0800

    books routes, index controller and view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get '/home', to: 'pages#home'
+
+#  get '/books', to: 'books#index'
+#  get '/books/new', to: 'books#new', as: 'new_book'
+#  post '/books', to: 'books#create'
+#  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+#  patch '/books/:id', to: 'books#update'
+#  get '/books/:id', to: 'books#show', as: 'book'
+#  delete '/books/:id', to: 'books#destroy'
+
+  resources :books
 end
