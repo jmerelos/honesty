f11ab38504ef90d640848831466a965b30ae6406
Joan Merelos <joan@egissys.com>
2017-05-21 16:40:14 +0800

    Show Book, Add new book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,16 @@
 class BooksController < ApplicationController
 	def index
+		@books = Book.all
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def edit
 	end
 end
