68c7e35aaefdca94ea2d2c75571c66d36f8a448a
Joan Merelos <joan@egissys.com>
2017-05-27 14:11:21 +0800

    CRUD Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,40 @@ class BooksController < ApplicationController
 		@book = Book.new
 	end
 
-	def edit
+	def create
+		@book = Book.new(book_params)
+		@book.author = Author.find(1)
+
+		if @book.save
+			flash[:success] = "Book was created successfully"
+			redirect_to books_path
+		else
+			render :new
+		end
 	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			flash[:success] = "Book was updated successfully"
+			redirect_to book_path(@book)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		Book.find(params[:id]).destroy
+		flash[:success] = "Book was deleted successfully"
+		redirect_to books_path
+	end
+
+	private
+		def book_params
+			params.require(:book).permit(:name, :description)
+		end
 end
